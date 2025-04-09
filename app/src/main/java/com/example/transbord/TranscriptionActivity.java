24cfb8943239336c598f8e1e7e863aad2e9339a5
abdul977 <abdulmuminibrahim74@gmail.com>
2025-04-09 11:45:05 -0700

    Fix: Add android.util.Log import to fix compilation errors

--- app/src/main/java/com/example/transbord/TranscriptionActivity.java
+++ app/src/main/java/com/example/transbord/TranscriptionActivity.java
@@ -1,6 +1,7 @@
 package com.example.transbord;
 
 import android.content.DialogInterface;
+import android.util.Log;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
